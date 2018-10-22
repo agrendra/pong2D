c7bc7e72374ffb3bd2ded6bf37eff281fd077e3e
agrendra <agerf11@gmail.com>
2018-10-22 13:27:49 +0700

    GameSelesai
    Audio, klik Btn, canvas dll kelar

---
+++ Pong2D/Assets/Scripts/HalamanManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HalamanManager : MonoBehaviour {
+
+	public bool isEscapeToExit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp (KeyCode.Escape)){
+			if(isEscapeToExit){
+				Application.Quit();
+			}else{
+				KembaliKeMenu();
+			}
+		}
+	}
+
+	public void MulaiPermainan(){
+		SceneManager.LoadScene("Main");
+	}
+
+	public void KembaliKeMenu(){
+		SceneManager.LoadScene("Menu");
+	}
+}
